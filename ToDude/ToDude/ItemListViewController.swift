35bc32c1baaf271eb8131ac2181555b4b29bae4f
Denis M <den@Deniss-Mac-mini.local>
2020-03-02 18:54:10 -0800

    Add item in ToDO list works

--- ToDude/ToDude/ItemListViewController.swift
+++ ToDude/ToDude/ItemListViewController.swift
@@ -10,8 +10,60 @@ import UIKit
 import CoreData
 
 class ItemListViewController: UITableViewController {
+  let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
   var items = [Item]()
-    override func viewDidLoad() {
+  
+  @IBAction func addItemButtonTapped(_ sender: UIBarButtonItem) {
+    // we need this in order to access the text field data outside of the 'addTextField' scope below
+    var tempTextField = UITextField()
+    
+    // create a UIAlertController object
+    let alertController = UIAlertController(title: "Add New Item", message: "", preferredStyle: .alert)
+    
+    // create a UIAlertAction object
+    let alertAction = UIAlertAction(title: "Done", style: .default) { (action) in
+      // create a new item from our Item core data entity (we pass it the context)
+      let newItem = Item(context: self.context)
+      
+      // if the text field text is not nil
+      if let text = tempTextField.text {
+        // set the item attributes
+        newItem.title = text
+        newItem.completed = false
+        
+        // append the item to our items array
+        self.items.append(newItem)
+        
+        // call our saveItems() method which saves our context and reloads the table
+        self.saveItems()
+      }
+    }
+    
+    alertController.addTextField { (textField) in
+      textField.placeholder = "Title"
+      tempTextField = textField
+    }
+    
+    // Add the action we created above to our alert controller
+    alertController.addAction(alertAction)
+    // show our alert on screen
+    present(alertController, animated: true, completion: nil)
+  }
+
+  func saveItems() {
+    // wrap our try statement below in a do/catch block so we can handle any errors
+    do {
+      // save our context
+      try context.save()
+    } catch {
+      print("Error saving context \(error)")
+    }
+    
+    // reload our table to reflect any changes
+    tableView.reloadData()
+  }
+  
+  override func viewDidLoad() {
         super.viewDidLoad()
 
         // Uncomment the following line to preserve selection between presentations
@@ -44,7 +96,6 @@ class ItemListViewController: UITableViewController {
   override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
     let item = items[indexPath.row]
     item.completed = !item.completed
-    <#code#>
   }
 
 
